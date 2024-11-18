eca45a3d4aaec81fc01e81d40a0fd12be7b0952f
Fheuef <fheuef27@gmail.com>
2024-11-18 19:58:08 +0100

    useless includeSource

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,5 @@ import { configDefaults, defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		exclude: [...configDefaults.exclude, "**/*sometimes.test.ts"],
-		includeSource: ["src/**/*.ts"],
 	},
 });
--- vitest_sometimes.config.ts
+++ vitest_sometimes.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		include: ["**/*sometimes.test.ts"],
-		includeSource: ["src/**/*.ts"],
 	},
 });
